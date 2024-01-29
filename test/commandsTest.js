f2bf79ab0c4653b33905bf7f3ca0f62d05038cc1
Exelupra <101408547+Exelupra@users.noreply.github.com>
2024-01-29 16:45:59 +0100

    Commit test + yml

---
+++ test/commandsTest.js
@@ -0,0 +1,5 @@
+// Exemple avec Jest
+test('Composant principal affiche correctement', () => {
+  // Créer un composant principal
+  // Vérifier que le composant principal s'affiche correctement
+});
